52be048ba0aae5c404eaa71858a76a8d0992f8b2
nikhilp132 <nikhilparadkar200@gmail.com>
2021-06-18 11:19:26 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-kytjwk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-kytjwk)
---
+++ index.js
@@ -0,0 +1,29 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+// for (var i = 1; i < 6; i++) {
+//   setTimeout(function() {
+//     console.log(i);
+//   }, 2000);
+// }
+
+// var a = 'abc';
+// var b = 1;
+// var c;
+// console.log(c);
+// var d = c + b;
+
+// console.log(d);
+
+// var a = 2;
+// var b = 1;
+
+if (a === b) {
+  console.log('true');
+} else {
+  console.log('false');
+}
